5f00eac0487e5bca5effbd5f0a6db548f6bfce90
dwright6633 <103792319+dwright6633@users.noreply.github.com>
2024-02-29 18:54:30 -0700

    initial Matrix view upload (doesn't work yet)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,27 +6,14 @@ namespace Mission_8.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Matrix()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
