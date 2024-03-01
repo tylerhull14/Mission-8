a051bba3ddc9c759732372b20a9f068b60d1dc6b
dwright6633 <103792319+dwright6633@users.noreply.github.com>
2024-03-01 14:05:44 -0700

    removed unneccessary

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,12 +7,6 @@ namespace Mission_8.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
@@ -24,10 +18,5 @@ namespace Mission_8.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mission_8.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
